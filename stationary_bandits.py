947b4737d85e3c726196d3e1b5ca1cbee272dce9
lawrence <245315189+lkliu7@users.noreply.github.com>
2026-01-10 22:44:24 -0800

    Comparison of various epsilon-greedy algorithms.

--- stationary_bandits.py
+++ stationary_bandits.py
@@ -7,40 +7,50 @@ CONFIG = {
     'time_steps': 1000,
     'runs': 2000,
     'epsilon': 0.01,
+    'epsilon_list': [0, 0.01, 0.1],
+    'progress_interval': 100,
 }
 
 n_bandits = CONFIG['n_bandits']
-steps = CONFIG['time_steps']
+time_steps = CONFIG['time_steps']
 runs = CONFIG['runs']
+epsilon_list = CONFIG['epsilon_list']
+progress_interval = CONFIG['progress_interval']
 
-ensemble_average_rewards = []
-ensemble_optimal_ratio = []
+cumulative_average_rewards = {}
+cumulative_optimal_ratio = {}
+for epsilon in epsilon_list:
+    ensemble_average_rewards = []
+    ensemble_optimal_ratio = []
 
-for run in range(runs):
-    bandit_means = np.random.randn(n_bandits)
-    means = np.zeros(n_bandits)
-    rewards = np.zeros(n_bandits)
-    actions = np.zeros(n_bandits)
-    optimal_bandit = bandit_means.argmax()
-    average_reward = []
-    optimal_ratio = []
+    for run in range(runs):
+        bandit_means = np.random.randn(n_bandits)
+        estimated_means = np.zeros(n_bandits)
+        total_rewards = np.zeros(n_bandits)
+        action_counts = np.zeros(n_bandits)
+        optimal_bandit = bandit_means.argmax()
+        average_reward = []
+        optimal_ratio = []
 
-    for t in range(steps):
-        greedy_choice = means.argmax()
-        choice = greedy_choice
-        actions[choice] += 1
-        reward = bandit_means[choice] + np.random.randn()
-        rewards[choice] += reward
-        means[choice] = rewards[choice] / actions[choice]
-        average_reward.append(rewards.sum() / (t + 1))
-        optimal_ratio.append(actions[optimal_bandit] / (t + 1))
+        for t in range(time_steps):
+            greedy_choice = estimated_means.argmax()
+            if random.random() < epsilon:
+                action = np.random.randint(n_bandits)
+            else:
+                action = greedy_choice
+            action_counts[action] += 1
+            reward = bandit_means[action] + np.random.randn()
+            total_rewards[action] += reward
+            estimated_means[action] = total_rewards[action] / action_counts[action]
+            average_reward.append(total_rewards.sum() / (t + 1))
+            optimal_ratio.append(action_counts[optimal_bandit] / (t + 1))
 
-    ensemble_average_rewards.append(average_reward)
-    ensemble_optimal_ratio.append(optimal_ratio)
-    if run % 100 == 99:
-        print(f'Completed run {run + 1}.')
+        ensemble_average_rewards.append(average_reward)
+        ensemble_optimal_ratio.append(optimal_ratio)
+        if run % progress_interval == progress_interval - 1:
+            print(f'Completed run {run + 1}.')
 
-ensemble_average_rewards = np.array(ensemble_average_rewards)
-ensemble_average_rewards = np.sum(ensemble_average_rewards, axis=0) / runs
-ensemble_optimal_ratio = np.array(ensemble_optimal_ratio)
-ensemble_optimal_ratio = np.sum(ensemble_optimal_ratio, axis=0) / runs
+    ensemble_average_rewards = np.array(ensemble_average_rewards)
+    cumulative_average_rewards[epsilon] = np.sum(ensemble_average_rewards, axis=0) / runs
+    ensemble_optimal_ratio = np.array(ensemble_optimal_ratio)
+    cumulative_optimal_ratio[epsilon] = np.sum(ensemble_optimal_ratio, axis=0) / runs
