f388f590bef03543bfa35b7160276bdca4de5cfc
lawrence <245315189+lkliu7@users.noreply.github.com>
2026-01-19 12:14:06 -0800

    Fixed tracked metrics.

--- stationary_bandits.py
+++ stationary_bandits.py
@@ -11,6 +11,7 @@ CONFIG = {
     'runs': 2000,
     'epsilon_list': [0, 0.01, 0.1],
     'progress_interval': 100,
+    'bias': 0,
 }
 
 n_bandits = CONFIG['n_bandits']
@@ -18,21 +19,22 @@ time_steps = CONFIG['time_steps']
 runs = CONFIG['runs']
 epsilon_list = CONFIG['epsilon_list']
 progress_interval = CONFIG['progress_interval']
+bias = CONFIG['bias']
 
 cumulative_average_rewards = {}
-cumulative_optimal_ratio = {}
+cumulative_optimal_frequency = {}
 for epsilon in epsilon_list:
     ensemble_average_rewards = []
-    ensemble_optimal_ratio = []
+    ensemble_optimal_frequency = []
 
     for run in range(runs):
         # Each bandit has a reward drawn from N(q*(a), 1) where q*(a) ~ N(0, 1)
         bandit_means = np.random.randn(n_bandits)
-        estimated_means = np.zeros(n_bandits)
+        estimated_means = np.zeros(n_bandits) + bias
         action_counts = np.zeros(n_bandits)
         optimal_bandit = bandit_means.argmax()
-        average_reward = []
-        optimal_ratio = []
+        rewards = []
+        optimal_actions = []
         running_total = 0
 
         for t in range(time_steps):
@@ -46,19 +48,22 @@ for epsilon in epsilon_list:
             reward = bandit_means[action] + np.random.randn()  # Reward ~ N(q*(a), 1)
             running_total += reward
             estimated_means[action] = estimated_means[action] + (reward - estimated_means[action]) / action_counts[action]  # Sample average
-            average_reward.append(running_total / (t + 1))  # Cumulative average reward
-            optimal_ratio.append(action_counts[optimal_bandit] / (t + 1))  # Fraction of optimal actions
+            rewards.append(reward)  # Record of all rewards
+            if action == optimal_bandit:
+                optimal_actions.append(1)
+            else:
+                optimal_actions.append(0)
 
-        ensemble_average_rewards.append(average_reward)
-        ensemble_optimal_ratio.append(optimal_ratio)
+        ensemble_average_rewards.append(rewards)
+        ensemble_optimal_frequency.append(optimal_actions)
         if run % progress_interval == progress_interval - 1:
             print(f'Completed run {run + 1}.')
 
     # Average performance across all runs for this epsilon value
     ensemble_average_rewards = np.array(ensemble_average_rewards)
     cumulative_average_rewards[epsilon] = np.sum(ensemble_average_rewards, axis=0) / runs
-    ensemble_optimal_ratio = np.array(ensemble_optimal_ratio)
-    cumulative_optimal_ratio[epsilon] = np.sum(ensemble_optimal_ratio, axis=0) / runs
+    ensemble_optimal_frequency = np.array(ensemble_optimal_frequency)
+    cumulative_optimal_frequency[epsilon] = np.sum(ensemble_optimal_frequency, axis=0) / runs
 
 for epsilon in epsilon_list:
     plt.plot(range(1, time_steps+1), cumulative_average_rewards[epsilon], label=f'epsilon = {epsilon}')
@@ -69,7 +74,7 @@ plt.legend()
 plt.show()
 
 for epsilon in epsilon_list:
-    plt.plot(range(1, time_steps+1), cumulative_optimal_ratio[epsilon], label=f'epsilon = {epsilon}')
+    plt.plot(range(1, time_steps+1), cumulative_optimal_frequency[epsilon], label=f'epsilon = {epsilon}')
 plt.xlabel('steps')
 plt.ylabel('optimal action ratio')
 plt.title(f'average fraction epsilon-greedy methods chose optimal action')
